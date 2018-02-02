99a43cf2b87aba461dc12fa5cf32e0c56f70c5c2
Bryan Lund <bryan.helge@gmail.com>
2018-02-02 09:50:55 -0700

    Setup basic iterator for documents page

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   moduleId: module.id,
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-  
+  documents: Document[] = [
+    { 
+      title: "My First Doc",
+      description: "this is a description",
+      file_url: "http://google.com",
+      updated_at: '02/05/18',
+      image_url: "http://google.com"
+    },
+    { 
+      title: "My Second Doc",
+      description: "this is a description",
+      file_url: "http://google.com",
+      updated_at: '02/05/18',
+      image_url: "http://google.com"
+    },
+    { 
+      title: "My Third Doc",
+      description: "this is a description",
+      file_url: "http://google.com",
+      updated_at: '02/05/18',
+      image_url: "http://google.com"
+    }
+  ]
 }
