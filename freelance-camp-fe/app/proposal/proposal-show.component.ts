9fb4fba4241c60bcd81e117d8bac2d93ded1c259
Bryan Lund <bryan.helge@gmail.com>
2018-02-03 19:09:57 -0700

    installed bootstrap 4 for styling and reinstalled node_modules

--- freelance-camp-fe/app/proposal/proposal-show.component.ts
+++ freelance-camp-fe/app/proposal/proposal-show.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { Proposal } from './proposal';
+
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-show',
   templateUrl: 'proposal-show.component.html'
 })
-export class ProposalShowComponent {}
+export class ProposalShowComponent implements OnInit {
+  id: number;
+  routeId: any;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.routeId = this.route.params.subscribe(
+      params => {
+        this.id = +params['id'];
+      }
+    )
+  }
+}
