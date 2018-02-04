71adc507eb0b38a0a2d427810cda73eacb710020
Bryan Lund <bryan.helge@gmail.com>
2018-02-04 09:54:36 -0700

    added card styling to documents page

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -14,21 +14,21 @@ export class DocumentsComponent {
       description: "this is a description",
       file_url: "http://google.com",
       updated_at: '02/05/18',
-      image_url: "http://google.com"
+      image_url: "https://media.licdn.com/mpr/mpr/AAIA_wDGAAAAAQAAAAAAAAmaAAAAJDZkMDI3MzNjLWEwY2UtNDBkZC1iMjFkLWI1MzFjNWQ1Zjk2YQ.jpg"
     },
     { 
       title: "My Second Doc",
       description: "this is a description",
       file_url: "http://google.com",
       updated_at: '02/05/18',
-      image_url: "http://google.com"
+      image_url: "https://themerkle.com/wp-content/uploads/2017/08/freelance.jpg"
     },
     { 
       title: "My Third Doc",
       description: "this is a description",
       file_url: "http://google.com",
       updated_at: '02/05/18',
-      image_url: "http://google.com"
+      image_url: "https://theopenmic.co/wp-content/uploads/2016/01/freelancer.jpg"
     }
   ]
 }
