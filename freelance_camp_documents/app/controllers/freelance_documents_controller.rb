0a826fe56adc8ddb91fc30506dbec6258dee7d23
Bryan Lund <bryan.helge@gmail.com>
2018-02-04 16:16:15 -0700

    updated seeds file to create mock content

--- freelance_camp_documents/app/controllers/freelance_documents_controller.rb
+++ freelance_camp_documents/app/controllers/freelance_documents_controller.rb
@@ -1,19 +1,16 @@
 class FreelanceDocumentsController < ApplicationController
   before_action :set_freelance_document, only: [:show, :update, :destroy]
 
-  # GET /freelance_documents
   def index
     @freelance_documents = FreelanceDocument.all
 
     render json: @freelance_documents
   end
 
-  # GET /freelance_documents/1
   def show
     render json: @freelance_document
   end
 
-  # POST /freelance_documents
   def create
     @freelance_document = FreelanceDocument.new(freelance_document_params)
 
@@ -24,7 +21,6 @@ class FreelanceDocumentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /freelance_documents/1
   def update
     if @freelance_document.update(freelance_document_params)
       render json: @freelance_document
@@ -33,18 +29,15 @@ class FreelanceDocumentsController < ApplicationController
     end
   end
 
-  # DELETE /freelance_documents/1
   def destroy
     @freelance_document.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_freelance_document
       @freelance_document = FreelanceDocument.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def freelance_document_params
       params.require(:freelance_document).permit(:title, :description, :file_url, :image_url)
     end
--- freelance_camp_documents/db/seeds.rb
+++ freelance_camp_documents/db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+  FreelanceDocument.create!(title: "document #{i}",
+                            description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, ",
+                            file_url: "https://docs.google.com/document/d/1VtAobb-sKFsj6GCf7Y_79zF7lm68eT2UgGWZcw4FMxc/edit?usp=sharing",
+                            image_url: 'http://www.tribalreport.com/wp-content/uploads/2017/01/freelance.jpg'
+                            )
+end
+
+
