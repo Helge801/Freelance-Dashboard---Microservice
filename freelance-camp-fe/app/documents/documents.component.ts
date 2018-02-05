4e8c6b32d0c52a7609335e46d004cd60a6b02679
Bryan Lund <bryan.helge@gmail.com>
2018-02-04 17:10:38 -0700

    set up connection for documents service to rails backend api for documents

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,35 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from './document';
+import { DocumentService } from './document.service';
+import { Observable } from 'rxjs/rx';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
   styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
-  documents: Document[] = [
-    { 
-      title: "My First Doc",
-      description: "this is a description",
-      file_url: "http://google.com",
-      updated_at: '02/05/18',
-      image_url: "https://media.licdn.com/mpr/mpr/AAIA_wDGAAAAAQAAAAAAAAmaAAAAJDZkMDI3MzNjLWEwY2UtNDBkZC1iMjFkLWI1MzFjNWQ1Zjk2YQ.jpg"
-    },
-    { 
-      title: "My Second Doc",
-      description: "this is a description",
-      file_url: "http://google.com",
-      updated_at: '02/05/18',
-      image_url: "https://themerkle.com/wp-content/uploads/2017/08/freelance.jpg"
-    },
-    { 
-      title: "My Third Doc",
-      description: "this is a description",
-      file_url: "http://google.com",
-      updated_at: '02/05/18',
-      image_url: "https://theopenmic.co/wp-content/uploads/2016/01/freelancer.jpg"
-    }
-  ]
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService;
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        )
+  }
+
 }
