ea636867bb1fd0300d146fd8a26f49ab2965a7e4
Bryan Lund <bryan.helge@gmail.com>
2018-02-05 08:25:35 -0700

    added proposals to database and udated and ran seeds

--- freelance_camp_proposal/db/seeds.rb
+++ freelance_camp_proposal/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |prop|
+  Proposal.create!( customer: "customer 1",
+                    portfolio_url: "http://bryan-lund.com",
+                    tools: "Ruby on rails, Angular 2, PostgreSQL",
+                    estimated_hours: (80 + prop),
+                    hourly_rate: 120,
+                    weeks_to_complete: 12,
+                    client_email: "bryan@gmail.com"
+                  )
+end
